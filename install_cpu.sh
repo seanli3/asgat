e3d107d545ea7366a24f3b6bc0994e086c1e2b59
Sean Li <sohey33@gmail.com>
2020-05-31 02:19:11 +1000

    fix

--- install_cpu.sh
+++ install_cpu.sh
@@ -3,7 +3,7 @@ pip3 install --user numpy
 pip3 install --user scipy
 pip3 install --user pygsp
 pip3 install --user scikit_learn
-pip3 install --user torch-scatter==2.0.4+cpu torch-sparse==0.6.4+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip3 install --user torch-scatter==2.0.4+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 pip3 install --user torch-sparse==0.6.4+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 pip3 install --user torch-cluster==1.5.4+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 pip3 install --user torch-spline-conv==1.2.0+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
