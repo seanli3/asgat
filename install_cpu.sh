d098793eba34ffb3d04280ab5f13e6c0ff48c58b
Sean Li <sohey33@gmail.com>
2020-05-31 00:48:01 +1000

    install scripts

---
+++ install_cpu.sh
@@ -0,0 +1,11 @@
+pip install torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install numpy
+pip install scipy
+pip install pygsp
+pip install scikit_learn
+pip install torch-scatter==2.0.4+cpu torch-sparse==latest+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip install torch-sparse==0.6.4+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip install torch-cluster==1.5.4+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip install torch-spline-conv==1.2.0+cpu -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip install torch-geometric
+python3 setup.py install
---
+++ install_gpu.sh
@@ -0,0 +1,12 @@
+pip3 install --user torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install --user numpy
+pip3 install --user scipy
+pip3 install --user pygsp
+pip3 install --user scikit_learn
+pip3 install --user torch-scatter==2.0.4+cu101 torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip3 install --user torch-sparse==0.6.4+cu101 -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip3 install --user torch-cluster==1.5.4+cu101 -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip3 install --user torch-spline-conv==1.2.0+cu101 -f https://pytorch-geometric.com/whl/torch-1.5.0.html
+pip3 install --user torch-geometric
+
+python3 setup.py install --user
