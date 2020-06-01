efcbeef6207c2e642725dfde3f486b2b4ac86176
Sean Li <sl0163@gadi-login-01.gadi.nci.org.au>
2020-06-01 12:24:35 +1000

    citeseer parameters

--- citeseer_create_parameters.py
+++ citeseer_create_parameters.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 parameters = []
-# dropout=0.95 hidden=24 heads=8 alpha=0.2 lr=0.0001 weight_decay=
-for wd in [0.0001, 0.00005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5]: # 8 done
-    parameters.append('--dataset=Citeseer --alpha=0.2 --lr=0.0001 --hidden=24 --heads=8 --dropout=0.95 --cuda --chebyshev_order=16 --early_stopping=10 --epochs=10000 --weight_decay={} --chebyshev_order=16\n'.format(str(wd)))
+# dropout=0.95 hidden=24 heads=8 alpha=0.2 lr=0.0001 weight_decay=0.001 cheby_order=12
+for order in [12]: # 12 done
+    parameters.append('--dataset=Citeseer --alpha=0.2 --lr=0.0001 --hidden=24 --heads=8 --dropout=0.95 --cuda --chebyshev_order=12 --early_stopping=10 --epochs=8000 --weight_decay=0.001\n'.format(str(order)))
 
 with open('citeseer_parameters.txt', 'w') as the_file:
     the_file.writelines(parameters)
