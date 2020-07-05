Added Gauss fitler and fix gradient issue

citation/deep_decimation.py
@@ -3,7 +3,6 @@ from model.layers import GraphSpectralFilterLayer, AnalysisFilter
 from model.spectral_filter import Graph
 import torch
 import torch.nn.functional as F
-from torch import nn
 from random import seed as rseed
 from numpy.random import seed as nseed
 from citation import get_planetoid_dataset, random_planetoid_splits, run
@@ -15,19 +14,19 @@ parser.add_argument('--runs', type=int, default=1)
 parser.add_argument('--epochs', type=int, default=8000)
 parser.add_argument('--alpha', type=float, default=0.2)
 parser.add_argument('--seed', type=int, default=729, help='Random seed.')
-parser.add_argument('--lr', type=float, default=0.005)
+parser.add_argument('--lr', type=float, default=0.001)
 parser.add_argument('--weight_decay', type=float, default=0.0005)
 parser.add_argument('--early_stopping', type=int, default=10)
 parser.add_argument('--hidden', type=int, default=24)
 parser.add_argument('--heads', type=int, default=8)
-parser.add_argument('--dropout', type=float, default=0.6)
+parser.add_argument('--dropout', type=float, default=0.5)
 parser.add_argument('--normalize_features', type=bool, default=True)
 parser.add_argument('--pre_training', action='store_true')
 parser.add_argument('--cuda', action='store_true')
 parser.add_argument('--chebyshev_order', type=int, default=16, help='Chebyshev polynomial order')
+parser.add_argument('--layers', type=int, default=1)
 parser.add_argument('--edge_dropout', type=float, default=0)
 parser.add_argument('--node_feature_dropout', type=float, default=0)
-parser.add_argument('--filter', type=str, default='analysis')
 args = parser.parse_args()
 
 print(args)
@@ -57,41 +56,38 @@ class Net(torch.nn.Module):
         self.G.estimate_lmax()
 
         self.analysis = GraphSpectralFilterLayer(self.G, dataset.num_node_features, args.hidden,
-                                                 dropout=args.dropout, out_channels=args.heads, filter=args.filter,
+                                                 dropout=args.dropout, out_channels=args.heads,
                                                  pre_training=args.pre_training, device='cuda' if args.cuda else 'cpu',
                                                  alpha=args.alpha, chebyshev_order=args.chebyshev_order)
-        # self.mlp = nn.Sequential(nn.Linear(args.hidden * args.heads, 128),
-        #                             nn.ReLU(inplace=True),
-        #                             nn.Linear(128, 64),
-        #                             nn.ReLU(inplace=True),
-        #                             nn.Linear(64, 32),
-        #                             nn.ReLU(inplace=True),
-        #                             nn.Linear(32, dataset.num_classes),
-        #                             nn.ReLU(inplace=True))
 
-        # self.W = torch.zeros(args.hidden * args.heads, dataset.num_classes)
-
-        self.synthesis = GraphSpectralFilterLayer(self.G, args.hidden * args.heads, dataset.num_classes, filter=args.filter,
+        self.synthesis = GraphSpectralFilterLayer(self.G, args.hidden * args.heads, dataset.num_classes,
                                                   device='cuda' if args.cuda else 'cpu', dropout=args.dropout,
                                                   out_channels=1, alpha=args.alpha, pre_training=False,
                                                   chebyshev_order=args.chebyshev_order)
 
+        self.layers = []
+        for _ in range(args.layers):
+            self.layers.append(GraphSpectralFilterLayer(self.G, args.hidden * args.heads, args.hidden,
+                                                        device='cuda' if args.cuda else 'cpu', dropout=args.dropout,
+                                                        out_channels=args.heads, alpha=args.alpha,
+                                                        pre_training=args.pre_training,
+                                                        chebyshev_order=args.chebyshev_order))
+
     def reset_parameters(self):
         self.analysis.reset_parameters()
-        # torch.nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        # for layer in self.mlp:
-        #     if hasattr(layer, 'reset_parameters'):
-        #         layer.reset_parameters()
         self.synthesis.reset_parameters()
+        for layer in self.layers:
+            layer.reset_parameters()
 
     def forward(self, data):
         x = data.x
         x = F.dropout(x, p=args.dropout, training=self.training)
         x = self.analysis(x)
+        for layer in self.layers:
+            x = F.dropout(x, p=args.dropout, training=self.training)
+            x = layer(x)
         x = F.dropout(x, p=args.dropout, training=self.training)
         x = F.elu(self.synthesis(x))
-        # x = x.mm(self.W)
-        # x = self.mlp(x)
         return F.log_softmax(x, dim=1)
 
 

citation/train_eval.py
@@ -71,7 +71,7 @@ def run(dataset, model, runs, epochs, lr, weight_decay, early_stopping,
             train(model, optimizer, data)
             eval_info = evaluate(model, data)
             eval_info['epoch'] = epoch
-            if epoch % 100 == 0:
+            if epoch % 10 == 0:
                 print(eval_info)
 
             if logger is not None:
@@ -84,6 +84,7 @@ def run(dataset, model, runs, epochs, lr, weight_decay, early_stopping,
                     model_test_acc = eval_info['test_acc']
                     model_val_loss = eval_info['val_loss']
                     model_val_acc = eval_info['val_acc']
+                    # torch.save(model.state_dict(), './best_gat_cora.pkl')
                 best_val_loss = min(eval_info['val_loss'], best_val_loss)
                 test_acc = max(eval_info['test_acc'], best_val_acc)
 

model/layers.py
@@ -4,6 +4,23 @@ import torch.nn.functional as F
 from .spectral_filter import Filter
 from torch_sparse import spmm
 import pygsp
+from math import sqrt
+
+
+class GaussFilter(nn.Module):
+    def __init__(self, k: int):
+        super(GaussFilter, self).__init__()
+        self.k = k
+        self.reset_parameters()
+        self.centers = torch.linspace(0, 2, k)
+
+    def reset_parameters(self):
+        self.bandwidths = nn.Parameter(torch.full([1, self.k], sqrt(2/self.k)))
+        self.gains = nn.Parameter(torch.ones(1, self.k))
+        self.amps = nn.Parameter(torch.full([1, self.k], 8.0))
+
+    def forward(self, x):
+        return self.gains.mul(torch.exp(-self.amps*(x.view(-1, 1) - self.centers).pow(2)/self.bandwidths.pow(2)))
 
 
 class AnalysisFilter(nn.Module):
@@ -33,7 +50,7 @@ class GraphSpectralFilterLayer(nn.Module):
     """
 
     def __init__(self, G, in_features, out_features, dropout, alpha, device,
-                 out_channels, chebyshev_order=16, pre_training=False):
+                 out_channels, chebyshev_order=16, pre_training=False, filter="analysis"):
         super(GraphSpectralFilterLayer, self).__init__()
         self.G = G
         self.device=device
@@ -47,12 +64,13 @@ class GraphSpectralFilterLayer(nn.Module):
         self.chebyshev_order = chebyshev_order
         self.leakyrelu = nn.LeakyReLU(self.alpha)
         self.N = G.n_vertices
-        self.filter_kernel = AnalysisFilter(out_channel=self.out_channels)
+        self.filter_type = filter
+        self.filter_kernel = AnalysisFilter(out_channel=self.out_channels) if self.filter_type == 'analysis' else GaussFilter(k=self.out_channels)
         self.filter = Filter(self.G, self.filter_kernel, chebyshev_order=self.chebyshev_order)
 
     def reset_parameters(self):
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        self.filter_kernel = AnalysisFilter(out_channel=self.out_channels)
+        self.filter_kernel = AnalysisFilter(out_channel=self.out_channels) if self.filter_type == 'analysis' else GaussFilter(k=self.out_channels)
         self.filter = Filter(self.G, self.filter_kernel, chebyshev_order=self.chebyshev_order)
 
         if self.pre_training:
@@ -88,9 +106,9 @@ class GraphSpectralFilterLayer(nn.Module):
                        / N / N / len(coefficients_list)
         h_primes = []
         for coefficients in coefficients_list:
-            coefficients_mask = coefficients._values() > overall_mean
-            attention_indices = coefficients._indices().T[coefficients_mask].T
-            attention_values = self.leakyrelu(coefficients._values()[coefficients_mask])
+            coefficients_mask = coefficients.values() > overall_mean
+            attention_indices = coefficients.indices().T[coefficients_mask].T
+            attention_values = self.leakyrelu(coefficients.values()[coefficients_mask])
             attention_values = torch.exp(attention_values).clamp(max=9e15)
             divisor = spmm(attention_indices,
                             attention_values,

model/spectral_filter.py
@@ -195,7 +195,10 @@ class Filter(nn.Module):
         r = []
 
         for i in range(nf):
-            r.append((twf_old * 0.5 * c[0][i] + twf_cur * c[1][i]))
+            r.append(
+                torch.sparse_coo_tensor(twf_old.indices(), twf_old.values() * 0.5 * c[0][i], twf_old.shape)
+                + torch.sparse_coo_tensor(twf_cur.indices(), twf_cur.values() * c[1][i], twf_cur.shape)
+            )
 
         factor = (2 / a1 * (G.L - a2 * self.signal)).coalesce()
 
@@ -209,9 +212,9 @@ class Filter(nn.Module):
             G.n_vertices)
 
         for k in range(2, M):
-            twf_new = torch.sparse_coo_tensor(fmt_index, fmt_value, [G.n_vertices, G.n_vertices]) - twf_old
+            twf_new = torch.sparse_coo_tensor(fmt_index, fmt_value, [G.n_vertices, G.n_vertices]).coalesce() - twf_old
             for i in range(nf):
-                r[i] = twf_new * c[k, i] + r[i]
+                r[i] = torch.sparse_coo_tensor(twf_new.indices(), twf_new.values() * c[k,i], twf_new.shape) + r[i]
 
             twf_old = twf_cur
             twf_cur = twf_new