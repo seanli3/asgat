fd28da433f71d2aba0f7373c276da89979d50047
Sean Li <s.li@data61.csiro.au>
2020-08-12 22:07:00 +1000

    Missed files

--- citation/__init__.py
+++ citation/__init__.py
@@ -1,8 +1,9 @@
-from .datasets import get_planetoid_dataset
-from .train_eval import random_planetoid_splits, run
+from .datasets import get_dataset
+from .train_eval import random_planetoid_splits, run, random_coauthor_amazon_splits
 
 __all__ = [
-    'get_planetoid_dataset',
+    'get_dataset',
     'random_planetoid_splits',
     'run',
+    'random_coauthor_amazon_splits'
 ]
