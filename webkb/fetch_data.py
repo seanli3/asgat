f4309ed2fccf0df084250616f824e0dbef04831b
Sean Li <s.li@data61.csiro.au>
2020-09-15 13:05:51 +1000

    Fetch data

--- webkb/fetch_data.py
+++ webkb/fetch_data.py
@@ -1,7 +1,7 @@
 from entities import get_dataset
 
 
-for data in ['AIFB', 'MUTAG', 'BGS', 'AM']:
+for data in ["Cornell", 'Texas', "Washington", "Wisconsin"]:
     print('fetching '+ data);
     get_dataset(data)
 
