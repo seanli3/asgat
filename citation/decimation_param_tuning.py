b0c91f4a337d4a163ccd8b56575af4927e4f018f
Sean Li <s.li@data61.csiro.au>
2020-09-29 11:49:25 +1000

    fix

--- citation/decimation_param_tuning.py
+++ citation/decimation_param_tuning.py
@@ -112,7 +112,7 @@ def decimation(args):
 best_parameters, best_values, _, _ = optimize(
  parameters=[{'name': 'dataset', 'type': 'fixed', 'value': args['dataset']},
     {'name': 'runs', 'type': 'fixed', 'value': 1},
-    {'name': 'epochs', 'type': 'fixed', 'value': 300},
+    {'name': 'epochs', 'type': 'fixed', 'value': 2000},
     {'name': 'alpha', "type": "range", "bounds": [0.1, 1.0]},
     {'name': 'seed', 'type': 'fixed', 'value': 729},
     {'name': 'lr', 'type': 'range', "type": "range", "bounds": [0.001, 0.01], "log_scale": True},
