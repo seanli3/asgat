f211f838b9a6efef5b06c40b2721e548ead0c581
Sean Li <li243@bracewell-login.cm.hpc>
2021-01-22 19:16:00 +1100

    update

--- citation/decimation_param_tuning.py
+++ citation/decimation_param_tuning.py
@@ -11,8 +11,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, required=True)
-parser.add_argument('--trials', type=int, default=20)
-parser.add_argument('--split', type=str, default='public')
+parser.add_argument('--trials', type=int, default=40)
+parser.add_argument('--split', type=str, default='full')
+parser.add_argument('--lcc', type=bool, default=False)
+parser.add_argument('--method', type=str, default='chebyshev')
 arg = parser.parse_args()
 
 args = {
@@ -29,11 +31,13 @@ args = {
     'dropout': 0.8,
     'normalize_features': True,
     'pre_training': True,
-    'cuda': True,
+    'cuda': False,
     'chebyshev_order': 13,
     'edge_dropout': 0,
     'node_feature_dropout': 0,
-    'filter': 'analysis'
+    'filter': 'analysis',
+    'method': arg.method,
+    'lcc': arg.lcc
 }
 
 
@@ -65,7 +69,7 @@ def decimation(args):
                                                      dropout=args['dropout'], out_channels=args['heads'], filter=args['filter'],
                                                      pre_training=args['pre_training'], device='cuda' if args['cuda'] else 'cpu',
                                                      alpha=args['alpha'], chebyshev_order=args['chebyshev_order'],
-                                                     k=args['k'])
+                                                     method=args['method'], k=args['k'])
             # self.mlp = nn.Sequential(nn.Linear(args['hidden * args['heads, 128),
             #                             nn.ReLU(inplace=True),
             #                             nn.Linear(128, 64),
@@ -80,7 +84,7 @@ def decimation(args):
             self.synthesis = GraphSpectralFilterLayer(self.G, args['hidden'] * args['heads'], dataset.num_classes, filter=args['filter'],
                                                       device='cuda' if args['cuda'] else 'cpu', dropout=args['dropout'],
                                                       out_channels=1, alpha=args['alpha'], pre_training=False,
-                                                      chebyshev_order=args['chebyshev_order'], k=args['k'])
+                                                      method=args['method'], chebyshev_order=args['chebyshev_order'], k=args['k'])
 
             if args['cuda']:
                 self.to('cuda')
@@ -107,7 +111,7 @@ def decimation(args):
             return F.log_softmax(x, dim=1), att1, att2
 
     use_dataset = lambda: get_dataset(args['dataset'], args['normalize_features'], edge_dropout=args['edge_dropout'],
-                                      permute_masks=None, cuda=args['cuda'], split=args['split'],
+                                      permute_masks=None, cuda=args['cuda'], split=args['split'],lcc=args['lcc'],
                                       node_feature_dropout=args['node_feature_dropout'], self_loop=args['self_loop'])
 
     return run(use_dataset, Net, args['runs'], args['epochs'], args['lr'], args['weight_decay'],
@@ -117,25 +121,27 @@ def decimation(args):
 best_parameters, best_values, _, _ = optimize(
  parameters=[{'name': 'dataset', 'type': 'fixed', 'value': args['dataset']},
     {'name': 'runs', 'type': 'fixed', 'value': 1},
-    {'name': 'epochs', 'type': 'fixed', 'value': 2000},
-    {'name': 'alpha', "type": "fixed", "value": 0.2},
+    {'name': 'epochs', 'type': 'fixed', 'value': 500},
+    {'name': 'alpha', "type": "range", "bounds": [0.1, 0.9]},
     {'name': 'seed', 'type': 'fixed', 'value': 729},
-    {'name': 'lr', 'type': 'range', "type": "range", "bounds": [0.001, 0.01], "log_scale": True},
+    {'name': 'lr', 'type': 'range', "type": "range", "bounds": [0.0001, 0.1], "log_scale": True},
     {'name': 'weight_decay', 'type': 'range', "bounds": [0.000001, 0.005], "log_scale": True},
-    {'name': 'patience', 'type': 'fixed', 'value': 100},
-    {'name': 'hidden', 'type': 'range', "bounds": [32, 512], "log_scale": True},
-    {'name': 'heads', 'type': 'range', "bounds": [1, 18]},
-    {'name': 'dropout', "type": "range", "bounds": [0.4, 0.9]},
-    {'name': 'self_loop', "type": "choice", "values": [True, False]},
-    {'name': 'normalize_features', 'type': 'fixed', 'value': True},
-    {'name': 'pre_training', 'type': 'fixed', 'value': False},
+    {'name': 'patience', 'type': 'fixed', 'value': 20},
+    {'name': 'hidden', 'type': 'range', "bounds": [16, 128], "log_scale": True},
+    {'name': 'heads', 'type': 'range', "bounds": [2, 18]},
+    {'name': 'dropout', "type": "range", "bounds": [0.1, 0.9]},
+    {'name': 'self_loop', "type": "choice", "values": [False ,True]},
+    {'name': 'normalize_features', "type": "fixed", "value": True},
+    {'name': 'pre_training', "type": "fixed", "value": False},
     {'name': 'cuda', 'type': 'fixed', 'value': args['cuda']},
     {'name': 'split', 'type': 'fixed', 'value': arg.split},
-    {'name': 'chebyshev_order', 'type': 'range', "bounds": [12, 18]},
+    {'name': 'lcc', 'type': 'fixed', 'value': arg.lcc},
+    {'name': 'method', 'type': 'fixed', 'value': arg.method},
+    {'name': 'chebyshev_order', 'type': 'range', "bounds": [10, 20]},
     {'name': 'edge_dropout', 'type': 'fixed', 'value': 0},
     {'name': 'node_feature_dropout', 'type': 'fixed', 'value': 0},
     {'name': 'filter', 'type': 'fixed', 'value': 'analysis'},
-    {'name': 'k', 'type': 'fixed', 'value': 5}],
+    {'name': 'k', 'type': 'range', 'bounds': [3, 20]}],
     evaluation_function=decimation,
     total_trials=arg.trials,
     minimize=False)
--- citation/fetch_data.py
+++ citation/fetch_data.py
@@ -1,7 +1,7 @@
 from citation import get_dataset
 
 
-for data in ['Cora', 'CiteSeer', 'PubMed', 'nell.0.1', 'nell.0.01', 'nell.0.001', 'CS', 'Physics', 'Flickr', 'Yelp']:
+for data in ['Cora', 'CiteSeer', 'PubMed', 'CS', 'Physics', 'Flickr', 'Yelp']:
     print('fetching '+ data)
-    get_dataset(data)
+    get_dataset(data, cuda=True)
 
